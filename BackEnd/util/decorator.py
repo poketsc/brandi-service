973f29d79357f279acaef17dfd59cf0be077a361
ulr0 <ulr0xb@gmail.com>
2021-05-28 16:21:11 +0900

    Refactor : 데코레이터 로직 수정 - 토큰이 없는 경우 에러를 처리하는 로직을 먼저 실행해서 depth 줄임

--- BackEnd/util/decorator.py
+++ BackEnd/util/decorator.py
@@ -14,36 +14,40 @@ def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         access_token = request.headers.get("Authorization")
-        if access_token is not None:
-            connection = None
-            try:
-                # 헤더에 있는 토큰을 디코드해서 payload에 담는다.
-                payload = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
+        
+        if access_token is None:
+            LoginRequiredError(LOGIN_REQUIRED, 401)
+            
+        connection = None
+        
+        try:
+            # 헤더에 있는 토큰을 디코드해서 payload에 담는다.
+            payload = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
 
-                # payload = {"account_id"    : user_info["account_id"]}
+            # payload = {"account_id"    : user_info["account_id"]}
 
-                account_dao = AccountDao()
-                connection  = connect_db()
+            account_dao = AccountDao()
+            connection  = connect_db()
 
-                account = account_dao.account_check(connection, payload)
+            account = account_dao.account_check(connection, payload)
 
-                if account["account_type_id"] != USER_ACCOUNT_TYPE:
-                    raise InvalidAccessError(UNAUTHORIZED_TOKEN, 401)
-
-                if account["is_deleted"] == 1:
-                    raise InvalidAccessError(UNAUTHORIZED_TOKEN, 401)
-
-                g.account_info = {"account_id" : payload["account_id"]}
-
-            except jwt.InvalidTokenError:
+            if account["account_type_id"] != USER_ACCOUNT_TYPE:
                 raise InvalidAccessError(UNAUTHORIZED_TOKEN, 401)
 
-            finally:
-                if connection is not None:
-                    connection.close()
+            if account["is_deleted"] == 1:
+                raise InvalidAccessError(UNAUTHORIZED_TOKEN, 401)
 
-        else:
-            raise LoginRequiredError(LOGIN_REQUIRED, 401)   
+            g.account_info = {"account_id" : payload["account_id"]}
+
+        except jwt.InvalidTokenError:
+            raise InvalidAccessError(UNAUTHORIZED_TOKEN, 401)
+
+        except Exception as e:
+            raise e
+
+        finally:
+            if connection is not None:
+                connection.close()
         
         return func(*args, **kwargs)
     return wrapper
